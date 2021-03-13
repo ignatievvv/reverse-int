6f2a9e6d537c821469ee5a9780b0eee05c003a45
ignatievvv <ramorusbadrobot@gmail.com>
2021-03-13 11:54:57 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    n = n + (-n)*2;
+  }
+  console.log(n);
+  let stringNew = String(n);
+  let arrayNew = stringNew.split('');
+  arrayNew.reverse();
+  stringNew = arrayNew.join('');
+  let numberNew = Number(stringNew);
+  return numberNew;
 }
